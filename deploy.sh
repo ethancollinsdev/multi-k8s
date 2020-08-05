cd7abfcaf1cad9a49d854832badf54e29bf9b43d
Ethan Collins <ethancollins@ibm.com>
2020-08-05 10:47:02 -0500

    ready for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t ethancollins/multi-client:latest -t ethancollins/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ethancollins/multi-server:latest -t ethancollins/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ethancollins/multi-worker:latest -t ethancollins/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push ethancollins/multi-client:latest
+docker push ethancollins/multi-client:$SHA
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push ethancollins/multi-server:latest
+docker push ethancollins/multi-server:$SHA
+
+docker push ethancollins/multi-worker:latest
+docker push ethancollins/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+
+kubectl set image deployments/client-deployment client=ethancollins/multi-client:$SHA
+kubectl set image deployments/server-deployment server=ethancollins/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=ethancollins/multi-worker:$SHA
